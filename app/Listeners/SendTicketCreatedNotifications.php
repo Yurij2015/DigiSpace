782e38a7a6082ff092ca092b36192df7d2b1a894
Yu <yurijmo@Yurijs-MacBook-Air.local>
2022-10-16 00:30:43 +0200

    GLOB-3 new ticket and notification

---
+++ app/Listeners/SendTicketCreatedNotifications.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\TicketCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendTicketCreatedNotifications
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\TicketCreated  $event
+     * @return void
+     */
+    public function handle(TicketCreated $event)
+    {
+        //
+    }
+}
