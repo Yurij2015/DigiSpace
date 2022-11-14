f051b1f8dae8f254671e6a5dbd66853088756016
Yu <yurijmo@Yurijs-MacBook-Air.local>
2022-11-14 01:53:24 +0100

    GLOB-18, add vueNotus, setup pages

--- resources/js/Pages/Admin/Posts/Index.vue
+++ resources/js/Pages/Admin/Posts/Index.vue
@@ -1,43 +1,41 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import PostAdmin from '@/Components/Admin/PostAdmin.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-
-
+import AdminNavbar from "@/components/Navbars/AdminNavbar.vue";
+import Sidebar from "@/components/Sidebar/Sidebar.vue";
+import HeaderStats from "@/components/Headers/HeaderStats.vue";
+import FooterAdmin from "@/components/Footers/FooterAdmin.vue";
 import {Head} from '@inertiajs/inertia-vue3';
 
 defineProps(['posts']);
 </script>
 
 <template>
-    <Head title="Admin Panel. Posts"><title>Posts. Admin</title>
-    </Head>
-    <AuthenticatedLayout>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="columns-2">
-                        <div style="line-height: 40px" class="font-bold">Posts</div>
-                        <div style="float: right">
+    <Head><title>Posts | Admin Panel</title></Head>
+    <div>
+        <sidebar/>
+        <AuthenticatedLayout>
+            <div class="relative md:ml-64 bg-blueGray-100">
+                <AdminNavbar/>
+                <HeaderStats/>
+                <div class="px-4 md:px-10 mx-auto w-full">
+                    <div class="flex flex-wrap">
+                        <div class="w-full xl:w-full mb-12 xl:mb-0 px-4 mt-2">
                             <a href="/admin/post-store">
                                 <div class="btn btn-blue text-center">
                                     Create
                                 </div>
                             </a>
+                            <PostAdmin
+                                v-for="post in posts"
+                                :key="post.id"
+                                :post="post"
+                            />
                         </div>
                     </div>
+                    <FooterAdmin/>
                 </div>
             </div>
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
-                    <PostAdmin
-                        v-for="post in posts"
-                        :key="post.id"
-                        :post="post"
-                    />
-                </div>
-            </div>
-        </div>
-    </AuthenticatedLayout>
-
+        </AuthenticatedLayout>
+    </div>
 </template>
