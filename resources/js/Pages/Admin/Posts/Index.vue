37bc6a3f0385afe35b6bdb92fd5b30a431fec9af
Yurij M <mokruy9999@gmail.com>
2022-11-17 22:03:49 +0100

    GLOB-18, setup Admin/Posts/Index Admin/Categories

--- resources/js/Pages/Admin/Posts/Index.vue
+++ resources/js/Pages/Admin/Posts/Index.vue
@@ -16,25 +16,36 @@ defineProps(['posts']);
         <div class="relative md:ml-64 bg-blueGray-100">
             <AdminNavbar/>
             <HeaderStats/>
-            <div class="px-4 md:px-10 mx-auto w-full">
-                <div class="flex flex-wrap">
-                    <div class="w-full xl:w-full mb-12 xl:mb-0 px-4 mt-2">
-                        <a href="/admin/post-store">
-                            <button
-                                class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                type="button">
-                                Create
-                            </button>
-                        </a>
-                        <PostAdmin
-                            v-for="post in posts"
-                            :key="post.id"
-                            :post="post"
-                        />
+            <div class="px-4 md:px-10 mx-auto w-full m-10">
+                <div
+                    class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+                    <div class="rounded-t bg-white mb-0 px-6 py-6">
+                        <div class="text-center flex justify-between">
+                            <h6 class="text-blueGray-700 text-xl font-bold">Posts</h6>
+                            <a href="/admin/post-store">
+                                <button
+                                    class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button">
+                                    Create
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                        <div class="flex flex-wrap">
+                            <div class="w-full xl:w-full mb-12 xl:mb-0 px-4 mt-2">
+
+                                <PostAdmin
+                                    v-for="post in posts"
+                                    :key="post.id"
+                                    :post="post"
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <FooterAdmin/>
             </div>
+            <FooterAdmin/>
         </div>
     </div>
 </template>
