32ffb47938924f11dc148e23cddd453b31a9231d
Yurij M <mokruy9999@gmail.com>
2023-01-05 02:14:51 +0100

    GLOB-21, add column to widget table, setup about page

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,7 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Page extends Model
 {
+    public function widgets(): BelongsToMany
+    {
+        return $this->belongsToMany(Widget::class);
+    }
 }
