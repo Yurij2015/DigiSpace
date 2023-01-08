9b0ab2b5eefc5896377ab457070c3e57579d67b6
Yurij M <mokruy9999@gmail.com>
2023-01-08 02:57:22 +0100

    GLOB-21, add info to widget and pageWidget seeders, get teaminfo in about contr

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Page;
+use App\Models\WidgetCategory;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -11,12 +12,23 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class AboutController extends Controller
 {
     public const GENERAL_INFO_WIDGET_CATEGORY = 7;
+    public const TEAM_INFO = 8;
+
 
     public function index(): Application|Factory|View
     {
         $aboutPageGeneralInfo = Page::with(['widgets' => function (BelongsToMany $query) {
             $query->where('widget_category_id', '=', self::GENERAL_INFO_WIDGET_CATEGORY);
         }])->where('slug', '=', 'about')->get();
-        return view('about.index', ['aboutPageGeneralInfo' => $aboutPageGeneralInfo]);
+        $teamInfoCategoryTitle = (WidgetCategory::where('id', '=', self::TEAM_INFO)->first())->description;
+        $teamInfo = Page::with(['widgets' => function (BelongsToMany $query) {
+            $query->where('widget_category_id', '=', self::TEAM_INFO);
+        }])->where('slug', '=', 'about')->first();
+        return view('about.index',
+            [
+                'aboutPageGeneralInfo' => $aboutPageGeneralInfo,
+                'teamInfo' => $teamInfo,
+                'teamInfoCategoryTitle' => $teamInfoCategoryTitle
+            ]);
     }
 }
