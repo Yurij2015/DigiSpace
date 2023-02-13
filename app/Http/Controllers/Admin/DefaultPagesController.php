e34cccd1a1608babe0e7a48383259b71f7847b27
Yurij M <mokruy9999@gmail.com>
2023-02-13 22:16:22 +0100

    GLOB-23, default pages table

--- app/Http/Controllers/Admin/DefaultPagesController.php
+++ app/Http/Controllers/Admin/DefaultPagesController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Page;
 use App\Models\Ticket;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -12,14 +14,21 @@ class DefaultPagesController extends Controller
 {
     public function index(): Response
     {
+//        dd($this->getPageData("promos")->widgets->count());
         return Inertia::render('Admin/DefaultPages/Index', [
             'defaultPages' => [
-                "about",
-                "services",
-                "pricing",
-                "promos",
-                "contact-us"
+                "about" => ['count' => $this->getPageData("about")->widgets->count()],
+                "services" => ['count' => $this->getPageData("services")->widgets->count()],
+                "pricing" => ['count' => $this->getPageData("pricing")->widgets->count()],
+                "promos" => ['count' => $this->getPageData("promos")->widgets->count()],
             ],
         ]);
     }
+
+
+    private function getPageData($slug)
+    {
+        return Page::with(['widgets'])->where('slug', '=', $slug)->first();
+    }
+
 }
