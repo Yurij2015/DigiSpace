556fa098ef79d50b130b55f4f0a0c889dbf34884
Yurij M <mokruy9999@gmail.com>
2023-02-15 23:56:50 +0100

    GLOB-23, show widgets of page

--- app/Http/Controllers/Admin/DefaultPagesController.php
+++ app/Http/Controllers/Admin/DefaultPagesController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Page;
+use App\Models\Widget;
 use App\Services\DefaultPageService;
+use App\Services\WidgetService;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -23,12 +26,19 @@ class DefaultPagesController extends Controller
         ]);
     }
 
-
-    public function show(Page $page): Response
+    public function show($pageId, WidgetService $widgetService): Response
     {
+        $page = Page::with(['widgets' => function (BelongsToMany $query) {
+            $query->with(['widgetCategory:id,title', 'widgetIcon']);
+        }])->where('id', '=', $pageId)->first();
+        if ($page) {
+            $widgetService->changeImgPathIfNullInWidgets($page->widgets);
+            return Inertia::render('Admin/DefaultPages/Show', [
+                'widgets' => $page->widgets,
+            ]);
+        }
         return Inertia::render('Admin/DefaultPages/Show', [
-            'page' => $page->widgets,
+            'widgets' => null,
         ]);
     }
-
 }
