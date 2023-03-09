2bc46a05cbfddbd1b2afc865b33fa1d11c267d0f
Yurij M <mokruy9999@gmail.com>
2023-03-09 21:25:45 +0100

    GLOB-29, move const in config

--- config/constants.php
+++ config/constants.php
@@ -1,4 +1,10 @@
 <?php
 return [
     'WIDGET_PER_PAGE' => 5,
+    'FOOTER_CATEGORY' => 11,
+    'PAGE_SUBMENU_FIRST' => 2,
+    'PAGE_SUBMENU_SECOND' => 3,
+    'PAGE_SUBMENU_THIRD' => 4,
+    'FOOTER_SUBMENU' => 5,
+    'NUMBER_POSTS_IN_MENU' => 6 // TODO - add to settings number of posts in menu
 ];
