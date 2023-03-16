a35f38a9c91bb379aebfd6c962c9e8c0f42a683b
Yurij M <mokruy9999@gmail.com>
2023-03-16 22:59:07 +0100

    GLOB-32, show service

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Service;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Inertia\Response;
+use Request;
 
 class ServiceController extends Controller
 {
@@ -14,4 +16,24 @@ class ServiceController extends Controller
         $services = Service::all();
         return Inertia::render('Admin/Services/Index', ['services' => $services]);
     }
+
+    final public function show(Service $service): Response
+    {
+        return Inertia::render('Admin/Services/Show', [
+            'service' => $service,
+        ]);
+    }
+
+    final public function update(Request $request, Service $service): RedirectResponse
+    {
+        //
+        return redirect(route('admin.services'));
+    }
+
+    final public function destroy(Service $service): RedirectResponse
+    {
+        //
+        return redirect(route('admin.services'));
+    }
+
 }
--- resources/js/Components/Admin/CardServices.vue
+++ resources/js/Components/Admin/CardServices.vue
@@ -43,7 +43,7 @@ const props = defineProps(['page_title', 'services']);
                         {{ item.title }}
                     </td>
                     <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        <Link :href="route('admin.services', item.id)" :title="item.title">
+                        <Link :href="route('admin.service-show', item.id)" :title="item.title">
                             <button
                                 class="bg-teal-500 text-white active:bg-teal-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                 type="button">
