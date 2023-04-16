7e3f51c3bef63b3fc42d30c8d8ef6c3d404c9154
Yurij M <mokruy9999@gmail.com>
2023-04-16 10:40:05 +0200

    GLOB-32, add PriceOfServices component

---
+++ app/View/Components/PriceOfServices.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\View\Component;
+
+class PriceOfServices extends Component
+{
+    /**
+     * The products data
+     *
+     * @var Collection
+     */
+    public Collection $products;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($products)
+    {
+        $this->products = $products;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return View|Closure|string
+     */
+    public function render(): View|string|Closure
+    {
+        return view('components.price-of-services');
+    }
+}
