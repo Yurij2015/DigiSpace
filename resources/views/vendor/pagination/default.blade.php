83bdaf6a45f3b8ae343837076d15ad0711f10b94
Yurij M <mokruy9999@gmail.com>
2023-04-16 23:19:59 +0200

    GLOB-32, paginate for services list

---
+++ resources/views/vendor/pagination/default.blade.php
@@ -0,0 +1,48 @@
+@if ($paginator->hasPages())
+    <div class="pagination__control"><a href="{{ $paginator->previousPageUrl() }}#services-table">Previous</a></div>
+    <nav>
+        <ul class="pagination__list">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                    <span aria-hidden="true">&lsaquo;</span>
+                </li>
+            @else
+                <li class="pagination__control">
+                    <a href="{{ $paginator->previousPageUrl() }}#services-table" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                </li>
+            @endif
+
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <li class="disabled" aria-disabled="true"><span>{{ $element }}</span></li>
+                @endif
+
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li class="active" aria-current="page"><span>{{ $page }}</span></li>
+                        @else
+                            <li><a href="{{ $url }}#services-table">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li class="pagination__control">
+                    <a href="{{ $paginator->nextPageUrl() }}#services-table" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                </li>
+            @else
+                <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <span aria-hidden="true">&rsaquo;</span>
+                </li>
+            @endif
+        </ul>
+    </nav>
+    <div class="pagination__control"><a href="{{ $paginator->nextPageUrl() }}#services-table">Next</a></div>
+@endif
