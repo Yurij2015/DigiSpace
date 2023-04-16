98bd7629c85221ba4d1ca59d2372467c045733e2
Yurij M <mokruy9999@gmail.com>
2023-04-16 23:43:03 +0200

    GLOB-32, add service page title bg

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -12,7 +12,6 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Http\Request;
 
 
 class ServiceController extends Controller
