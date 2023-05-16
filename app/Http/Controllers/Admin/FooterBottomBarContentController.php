41e644620b35e85986695314b1cd3915f55168bf
Yurij M <mokruy9999@gmail.com>
2023-05-17 00:47:40 +0200

    GLOB-34, footer bottom bar content

--- app/Http/Controllers/Admin/FooterBottomBarContentController.php
+++ app/Http/Controllers/Admin/FooterBottomBarContentController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\FooterBottomBarContent;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class FooterBottomBarContentController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        $footerBottomBarContent = FooterBottomBarContent::all()->first();
+        return Inertia::render(
+            'Admin/FooterBottomBarSettings/Index',
+            ['footerBottomBarContent' => $footerBottomBarContent]
+        );
+    }
 }
--- app/Http/Controllers/Admin/HeaderNavBarContentController.php
+++ app/Http/Controllers/Admin/HeaderNavBarContentController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\HeaderNavBarSettingsSaveRequest as SaveRequest;
+use App\Models\HeaderNavBarContent;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Inertia\Response;
+
 
 class HeaderNavBarContentController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        $headerNavBarContent = HeaderNavBarContent::all()->first();
+        return Inertia::render(
+            'Admin/HeaderNavBarSettings/Index',
+            ['headerNavBarContent' => $headerNavBarContent]
+        );
+    }
+
+    final public function update(HeaderNavBarContent $headerNavBarContent, SaveRequest $saveRequest): RedirectResponse
+    {
+        $headerNavBarContent->update($saveRequest->all());
+        return redirect(route('admin.top-bar-settings'))->with('message', 'Service Updated Successfully');
+    }
 }
