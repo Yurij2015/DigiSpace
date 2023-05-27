335576dfd6b81c244d7e6f8a90d2227718e93aaf
YuriiM <mokruy9999@gmail.com>
2023-05-27 10:26:00 +0200

    DS-38, change get groupes posts sql query

--- app/Repositories/BlogRepository.php
+++ app/Repositories/BlogRepository.php
@@ -10,10 +10,12 @@ class BlogRepository
 {
     public function getGroupedPosts(): Collection
     {
-        return DB::table('posts')
-            ->select(DB::raw('YEAR(created_at) as year, MONTH(created_at) as month, MONTHNAME(created_at) as month_name, count(*) as post_count'))
-            ->groupBy('year', 'month', 'month_name')->orderBy('year', 'asc')
-            ->orderBy('month', 'asc')->get();
+        return Post::selectRaw('YEAR(created_at) as year,
+        MONTH(created_at) as month, MONTHNAME(created_at) as month_name, count(*) as post_count')
+            ->groupBy('year', 'month', 'month_name')
+            ->orderBy('year', 'asc')
+            ->orderBy('month', 'asc')
+            ->get();
     }
 
     public function getArchivedPosts(int $year, int $month): Collection
