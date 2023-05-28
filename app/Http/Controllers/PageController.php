3e9d46125ed2c9c5fd22e5af0b967cecbed4b2c1
YuriiM <mokruy9999@gmail.com>
2023-05-28 17:25:07 +0200

    DS-39, set status code

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,22 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\MenuItem;
-use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\Response;
 use Illuminate\View\View;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class PageController extends Controller
 {
     /**
      * Display the specified resource.
      * @param string $slug
-     * @return Application|\Illuminate\Http\RedirectResponse|
+     * @return Response|View
      */
-    public function show(string $slug): RedirectResponse|View
+    public function show(string $slug): Response | View
     {
         $menuItemPage = MenuItem::with('pages')->where('slug', $slug)->firstOrFail();
         if ($menuItemPage->pages->count() === 0) {
-            return redirect(route('error-404'));
+            return response()->view('errors.page-not-found')->setStatusCode(404);
         }
         return view('pages.show', [
             'page' => $menuItemPage->pages->first(),
