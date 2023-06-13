d1383496808bcfffd815ff012b0e947a62810c1b
YuriiM <mokruy9999@gmail.com>
2023-06-13 11:35:12 +0200

    DS-47, add routes on home page

--- resources/views/components/technologies.blade.php
+++ resources/views/components/technologies.blade.php
@@ -2,22 +2,26 @@
     <div class="container">
         <div class="row row-30 align-items-sm-center">
             <div class="col-sm-6 col-md-3 wow fadeIn">
-                <a class="link-image" href="#">
+                <a class="link-image"
+                   href="{{ route('pages.page', 'host-your-website-on-ubuntu-servers-with-digispace') }}">
                     <img src="{{ asset('images/bash-about-page.png') }}" alt="bash" width="100"/>
                 </a>
             </div>
             <div class="col-sm-6 col-md-3 wow fadeIn">
-                <a class="link-image" href="#">
+                <a class="link-image"
+                   href="{{ route('pages.page', 'opening-incredible-opportunities-php-is-our-1-choice-for-website-development') }}">
                     <img src="{{ asset('images/php-about-page.png') }}" alt="php" width="100"/>
                 </a>
             </div>
             <div class="col-sm-6 col-md-3 wow fadeIn">
-                <a class="link-image" href="#">
+                <a class="link-image"
+                   href="{{ route('pages.page', 'websites-and-apps-developed-using-javascript-frameworks') }}">
                     <img src="{{ asset('images/javascript-about-page.png') }}" alt="javascript" width="100"/>
                 </a>
             </div>
             <div class="col-sm-6 col-md-3 wow fadeIn">
-                <a class="link-image" href="#">
+                <a class="link-image"
+                   href="{{ route('pages.page', 'mysql-website-development-the-technology-which-became-legendary') }}">
                     <img src="{{ asset('images/mysql-about-page.png') }}" alt="mysql" width="100"/>
                 </a>
             </div>
