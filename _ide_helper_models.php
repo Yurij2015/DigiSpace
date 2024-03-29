eac5d56d371b783667d5b4cfa958c6b42df6a0be
YuriiM <mokruy9999@gmail.com>
2024-03-29 21:04:49 +0100

    update composer, DS-55

---
+++ _ide_helper_models.php
@@ -0,0 +1,35 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models\Portfolio{
+/**
+ * App\Models\Portfolio\Pflink
+ *
+ * @property int $id
+ * @property int|null $item_id
+ * @property string|null $fa_icon
+ * @property string|null $href
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|PfLink newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PfLink newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PfLink query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PfLink whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PfLink whereFaIcon($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PfLink whereHref($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PfLink whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PfLink whereItemId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PfLink whereUpdatedAt($value)
+ */
+	class PfLink extends \Eloquent {}
+}
+
