4615c476e5da4d1445b0a805497b3f16c0b86c73
YuriiM <mokruy9999@gmail.com>
2024-07-18 19:05:42 +0200

    task(DS), add plerdy script on site, DS-69

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -69,6 +69,17 @@
 
     gtag('config', 'G-5SMHNENJQK');
 </script>
+<!-- BEGIN PLERDY CODE -->
+<script type="text/javascript" defer data-plerdy_code='1'>
+    var _protocol="https:"==document.location.protocol?"https://":"http://";
+    _site_hash_code = "ca16216c56a35002f0edb72556d4b626",_suid=52604, plerdyScript=document.createElement("script");
+    plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",
+        plerdyScript.src="https://a.plerdy.com/public/js/click/main.js?v="+Math.random();
+    var plerdymainscript=document.querySelector("[data-plerdymainscript='plerdymainscript']");
+    plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);
+    try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}
+</script>
+<!-- END PLERDY CODE -->
 <!-- Page Loader-->
 <div id="page-loader">
     <div class="page-loader-body">
